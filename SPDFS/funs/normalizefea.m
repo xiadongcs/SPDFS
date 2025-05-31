de3a6fffb6c1ed2e4c56a25ede42accff9d427c0
Xia Dong <50310204+xiadongcs@users.noreply.github.com>
2025-05-31 16:23:18 +0800

    Delete SPDFS directory

--- SPDFS/funs/normalizefea.m
+++
@@ -1,9 +0,0 @@
-%% Preprocessing
-function [newfea] = normalizefea(num, oldfea)
-X0 = oldfea;
-mX0 = mean(X0);
-X0 = X0 - ones(num,1)*mX0;
-scal = 1./sqrt(sum(X0.*X0)+eps);
-scalMat = sparse(diag(scal));
-newfea = X0*scalMat;
-end
